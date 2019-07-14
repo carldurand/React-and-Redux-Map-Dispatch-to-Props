365cd43e390e2a33cb2716694a43f609fb6316db
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2019-07-13 21:46:51 -0400

    React and Redux: Map Dispatch to Props

--- react.js
+++ react.js
@@ -1,8 +1,15 @@
-const state = [];
-const mapStateToProps = (state) =>{
-    return{
-        messages:state
-    }
-}
+const addMessage = (message) => {
+  return {
+    type: 'ADD',
+    message: message
+  }
+};
 
 // change code below this line
+const mapDispatchToProps = (dispatch) =>{
+  return{
+    submitNewMessage:(message)=>{
+      dispatch(addMessage(message))
+    }
+  }
+}
